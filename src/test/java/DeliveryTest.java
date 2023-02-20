9e49e3be304a28be1f6eb4cf67924779b0499742
OPCSenator <opc_senator4@mail.ru>
2023-02-21 00:27:36 +0300

    Добавил аллюр

--- src/test/java/DeliveryTest.java
+++ src/test/java/DeliveryTest.java
@@ -1,9 +1,8 @@
 import com.codeborne.selenide.Configuration;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.Keys;
-
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 
 import java.time.Duration;
 
@@ -13,6 +12,16 @@ import static com.codeborne.selenide.Selenide.$x;
 import static com.codeborne.selenide.Selenide.open;
 
 class DeliveryTest {
+
+    @BeforeAll
+    static void setUpAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
+    @AfterAll
+    static void tearDownAll() {
+        SelenideLogger.removeListener("allure");
+    }
 
     @BeforeEach
     void setup() {
